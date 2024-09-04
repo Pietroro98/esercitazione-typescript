63a7a844f0977193bdeb3fa53aba2d98cc213c7b
Pietroro98 <pietroro98@gmail.com>
2024-09-04 16:53:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+// es1
+// TypeScript (TS) è un linguaggio di programmazione open-source  che estende JavaScript con la possibilità di aggiungere tipi di dati statici alle variabili, funzioni e oggetti.
+//erve a migliorare la qualità e la sicurezza del codice JavaScript, riducendo sopratutto gli errori
+// risolve il problema della mancanza di tipizzazione statica in JavaScript, che può portare a errori risolve il problema della mancanza di tipizzazione statica in JavaScript, che può portare a errori nel codice
+// La principale differenza tra TypeScript e JavaScript è che TypeScript può essere compilato in JavaScript, ma non viceversa:
+// TypeScript
+let nome = 'Mario';
+console.log(nome);
+// JavaScript
+let nome2 = 'Mario';
+console.log(nome2);
+// 2. Il compilatore TS
+// Il compilatore TypeScript (tsc) è un tool che converte il codice TypeScript in JavaScript. È necessario perché il codice TypeScript non può essere eseguito direttamente dai browser o dai motori JavaScript:
+// 3. La Type Inference
+// La Type Inference è una funzionalità di TypeScript che consente di dedurre i tipi di dati delle variabili e delle funzioni senza la necessità di specificarli esplicitamente:
+// Type Inference
+let nome1 = 'Mario';
+console.log(nome1); // string
+// 4. Il tipo ‘any’
+// Il tipo ‘any’ è un tipo di dati speciale in TypeScript che  ci permette di cambiare il valore delle variabnile con qualsiasi valore senza problemi, ma non va fatto perche senno l'editor non ci da una mano negli errori
+let variabile = 'Mario';
+variabile = 42;
+// 5. Il tipo Union
+// Il tipo Union è un tipo di dati che rappresenta una combinazione di due o più tipi di dati.
+let test = 50;
+test = "Pietro";
+test = 100;
+let test2 = "pietro";
+let people1 = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    modules: ['TS', 'JS'],
+};
+people1.region = "italia"; // con il ? la variabile regione è opzionale e si può aggiungere successivamente
+console.log(people1);
---
+++ index.ts
@@ -0,0 +1,64 @@
+
+                                 // es1
+// TypeScript (TS) è un linguaggio di programmazione open-source  che estende JavaScript con la possibilità di aggiungere tipi di dati statici alle variabili, funzioni e oggetti.
+
+//erve a migliorare la qualità e la sicurezza del codice JavaScript, riducendo sopratutto gli errori
+
+// risolve il problema della mancanza di tipizzazione statica in JavaScript, che può portare a errori risolve il problema della mancanza di tipizzazione statica in JavaScript, che può portare a errori nel codice
+
+// La principale differenza tra TypeScript e JavaScript è che TypeScript può essere compilato in JavaScript, ma non viceversa:
+// TypeScript
+
+let nome: string = 'Mario';
+console.log(nome);
+
+// JavaScript
+let nome2 = 'Mario';
+console.log(nome2);
+
+// 2. Il compilatore TS
+// Il compilatore TypeScript (tsc) è un tool che converte il codice TypeScript in JavaScript. È necessario perché il codice TypeScript non può essere eseguito direttamente dai browser o dai motori JavaScript:
+
+// 3. La Type Inference
+
+// La Type Inference è una funzionalità di TypeScript che consente di dedurre i tipi di dati delle variabili e delle funzioni senza la necessità di specificarli esplicitamente:
+
+// Type Inference
+let nome1 = 'Mario';
+console.log(nome1); // string
+
+// 4. Il tipo ‘any’
+
+// Il tipo ‘any’ è un tipo di dati speciale in TypeScript che  ci permette di cambiare il valore delle variabnile con qualsiasi valore senza problemi, ma non va fatto perche senno l'editor non ci da una mano negli errori
+let variabile: any = 'Mario';
+variabile = 42;
+
+// 5. Il tipo Union
+
+// Il tipo Union è un tipo di dati che rappresenta una combinazione di due o più tipi di dati.
+let test: string | number = 50;
+test = "Pietro";
+test = 100;
+let test2: string | string[] | undefined = "pietro";
+
+// 6. Le Interfaces in TS
+
+// Le Interfaces in TypeScript sono utilizzate per definire la struttura di un oggetto o di una funzione.
+
+interface People {
+    firstName: string // obbligatorio
+    lastName: string // obbligatorio
+    modules: string[] // obbligatorio
+    region?: string // opzionale
+  }
+  
+
+  let people1: People = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    modules: ['TS', 'JS'],
+  };
+
+  people1.region = "italia" // con il ? la variabile regione è opzionale e si può aggiungere successivamente
+
+  console.log(people1);
